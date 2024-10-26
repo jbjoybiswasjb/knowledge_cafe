e162fc3edea59367aa378647bd2de4956c873d71
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-10-26 10:00:03 +0600

    Added google font.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header>
             <div className="w-4/5 mx-auto">
-                <h1 className='bg-yellow-100 font-bold text-3xl'>
+                <h1 className='bg-yellow-100 text-mainHeading font-bold text-3xl'>
                     Knowledge Cafe
                 </h1>
             </div>
